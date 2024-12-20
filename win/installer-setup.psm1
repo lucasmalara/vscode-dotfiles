edc0d21f693a95f8f8b0c55d6628359274dae3c8
Lucas <malara.career.d9mwd@slmail.me>
2024-12-20 20:38:05 +0100

    Added powershell script module.

---
+++ win/installer-setup.psm1
@@ -0,0 +1,38 @@
+function Install-Extension {
+    param (
+        [ValidateNotNullOrEmpty()][string]$extensionId,
+        $profileName = $null
+    )
+
+    $install = ""
+    if ($null -ne $profileName) {
+        $install = "code --profile $profileName -and "
+    }
+    $install += "code --install-extension $extensionId"
+    Invoke-Expression $install
+}
+
+function Install-GroupedExtensions {
+    param (
+        [ValidateNotNullOrEmpty()][Array]$groupedExtensions, 
+        $profileName = $null
+    )
+    
+    foreach($id in $groupedExtensions) {
+        Install-Extension $id $profileName
+    }
+}
+
+function Install-GroupOfGroupedExtensions {
+    param (
+        [ValidateNotNullOrEmpty()][Array]$groupOfGroupedExtensions, 
+        $profileName = $null
+    )
+
+    foreach($groupedExtensions in $groupOfGroupedExtensions) {
+        Install-GroupedExtensions $groupedExtensions $profileName
+    }
+}
+Export-ModuleMember -Function Install-Extension,
+    Install-GroupedExtensions, 
+    Install-GroupOfGroupedExtensions
